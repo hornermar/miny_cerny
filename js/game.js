513aab9e4a1b5d0e50630af78ca85679f57c2211
hornermar <hornerova.m@gmail.com>
2025-10-30 20:42:52 +0100

    test cell size on phone

--- js/game.js
+++ js/game.js
@@ -41,11 +41,11 @@ function recalculateCellSizeAndGridWidth() {
   
   if (isMobile) {
     if (isVerySmallScreen) {
-      minCellSize = Math.min(GRID.MIN_CELL_SIZE, calculatedCellSize);
+      minCellSize = Math.min(20, calculatedCellSize);
       maxCellSize = calculatedCellSize;
     } else if (isHighDensity) {
-      minCellSize = GRID.MIN_CELL_SIZE + 8;
-      maxCellSize = GRID.MAX_CELL_SIZE + 8;
+      minCellSize = GRID.MIN_CELL_SIZE + 12;
+      maxCellSize = GRID.MAX_CELL_SIZE + 16;
     } else {
       minCellSize = GRID.MIN_CELL_SIZE + 4;
       maxCellSize = GRID.MAX_CELL_SIZE + 4;
